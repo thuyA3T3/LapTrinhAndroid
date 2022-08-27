1ae49ccfa48040743410677b5fd461cf2eba0246
thuyA3T3 <thuydao13042002@gmail.com>
2022-08-27 23:13:49 +0700

    push the code

---
+++ appcuaThuy/app/src/main/java/com/dct/appcuathuy/MainActivity2.java
@@ -0,0 +1,20 @@
+package com.dct.appcuathuy;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try
+        {
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException e){}
+        setContentView(R.layout.activity_main2);
+
+    }
+}
